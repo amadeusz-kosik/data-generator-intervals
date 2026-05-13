e6ec7d7676977d9ce69101aae5d563bc97f5214e
Amadeusz Kosik <tappers_teams.5d@icloud.com>
2026-05-13 22:52:07 +0200

    Add application entry point.

--- src/main/scala/data/generator/intervals/Main.scala
+++ src/main/scala/data/generator/intervals/Main.scala
@@ -1,6 +1,32 @@
 package data.generator.intervals
 
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+
 
 object Main extends App {
+  private val DatabaseRowsCount   = 10000000L
+  private val DatabaseGroupsCount = 10
+  private val TargetPartitionsCount = 8
 
+  private implicit val sparkSession: SparkSession = SparkSession.builder()
+    .appName("Data generator for intervals data")
+    .getOrCreate()
+
+  private val outputPath = "/mnt/data/synthetic"
+
+  Map[String, (Long, Int) => DataFrame](
+    "databaseUniformFlat"         -> DataSuites.databaseUniformFlat,
+    "databaseUniformStacked"      -> DataSuites.databaseUniformStacked,
+    "databaseUniformHeavyStacked" -> DataSuites.databaseUniformHeavyStacked,
+    "databaseSkewedFlat"          -> DataSuites.databaseSkewedFlat,
+    "databaseSkewedStacked"       -> DataSuites.databaseSkewedStacked,
+    "querySparse"                 -> DataSuites.querySparse,
+    "queryDense"                  -> DataSuites.queryDense
+  ) foreach { case (name, callback) =>
+    callback(DatabaseRowsCount, DatabaseGroupsCount)
+      .repartition(TargetPartitionsCount)
+      .write
+      .mode(SaveMode.Overwrite)
+      .parquet(f"$outputPath/$name.parquet")
+  }
 }
