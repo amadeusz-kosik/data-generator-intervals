e483a0fb95abcb41807ab0d7830323545965d417
Amadeusz Kosik <tappers_teams.5d@icloud.com>
2026-05-15 12:34:03 +0200

    Add data size tiers to synthetic datasets.

--- src/main/scala/data/generator/intervals/Main.scala
+++ src/main/scala/data/generator/intervals/Main.scala
@@ -4,17 +4,10 @@ import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 
 object Main extends App {
-  private val DatabaseRowsCount   = 10000000L
   private val DatabaseGroupsCount = 10
   private val TargetPartitionsCount = 8
 
-  private implicit val sparkSession: SparkSession = SparkSession.builder()
-    .appName("Data generator for intervals data")
-    .getOrCreate()
-
-  private val outputPath = "/mnt/data/synthetic"
-
-  Map[String, (Long, Int) => DataFrame](
+  private val Datasets = Map[String, (Long, Int) => DataFrame](
     "databaseUniformFlat"         -> DataSuites.databaseUniformFlat,
     "databaseUniformStacked"      -> DataSuites.databaseUniformStacked,
     "databaseUniformHeavyStacked" -> DataSuites.databaseUniformHeavyStacked,
@@ -24,11 +17,27 @@ object Main extends App {
     "queryDense"                  -> DataSuites.queryDense,
     "querySkewedDense"            -> DataSuites.querySkewedDense,
     "queryDummy"                  -> DataSuites.queryDummy
-  ) foreach { case (name, callback) =>
-    callback(DatabaseRowsCount, DatabaseGroupsCount)
-      .repartition(TargetPartitionsCount)
-      .write
-      .mode(SaveMode.Overwrite)
-      .parquet(f"$outputPath/$name.parquet")
+  )
+
+  private val DatabaseSizes = Array(
+      100000L,
+     1000000L,
+    10000000L
+  )
+
+  private implicit val sparkSession: SparkSession = SparkSession.builder()
+    .appName("Data generator for intervals data")
+    .getOrCreate()
+
+  private val outputPath = "/mnt/data/synthetic"
+
+  Datasets.foreach { case (name, callback) =>
+    DatabaseSizes.foreach { (databaseRowsCount) =>
+      callback(databaseRowsCount, DatabaseGroupsCount)
+        .repartition(TargetPartitionsCount)
+        .write
+        .mode(SaveMode.Overwrite)
+        .parquet(f"$outputPath/$databaseRowsCount/$name.parquet")
+    }
   }
 }
